6bca851e1fc9d7ade3428e61d2850043f7b48f3f
cristinayyds <95115820+cristinayyds@users.noreply.github.com>
2025-07-19 20:37:40 +0800

    final

--- text_to_img.py
+++ text_to_img.py
@@ -75,7 +75,7 @@ with tab1:
 
     # 翻译中文为英文
     def translate_to_english(text):
-        return GoogleTranslator(source='chinese', target='english').translate(text)
+        return GoogleTranslator(source='auto', target='en').translate(text)
     def generate_image(prompt):
         url = "https://api.lightai.io/ideogram/generate"
         payload = json.dumps({
